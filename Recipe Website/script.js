10e5e9d2b66f3078b0c150a267e5a2f27781ba58
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-07-25 14:21:20 -0600

    made corrections on code

--- Recipe Website/script.js
+++ Recipe Website/script.js
@@ -1,8 +1,19 @@
-function countCheckboxes() {
-    let a =document.forms["main"];
-    let x = a.querySelectorAll('input[type = "checkbox"]: checked');
-    alert(x.lenght);
+function printCheckedIngredients() {
+    let a = document.forms["main"];
+    let checkboxes = a.querySelectorAll('input[type="checkbox"]');
+    let checkedIngredients = [];
+
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            let label = checkbox.nextElementSibling.textContent;
+            checkedIngredients.push(label);
+        }
+    });
+
+    if (checkedIngredients.length > 0) {
+        let ingredientsList = checkedIngredients.join("\n");
+        alert("Checked Ingredients:\n" + ingredientsList);
+    } else {
+        alert("No ingredients are checked.");
+    }
 }
-
-
-
