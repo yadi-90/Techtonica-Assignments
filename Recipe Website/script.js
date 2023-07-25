14f855b71d1a61ebcdbdaf4d0146a855d0177914
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-07-25 13:43:43 -0600

    change form

--- Recipe Website/script.js
+++ Recipe Website/script.js
@@ -1,33 +1,8 @@
-let theForm = document.theForm;
-
-
-function btnRemoveIngredientsClick() {
-    let options = theForm.theIngredient.options;
-    if (options[2].text == "Ingredient"){
-        options[2] = null;
-    } else {
-        alert (" There is no ingredient in list!");
-        }
-    }
-    function btnAddIngredientsClick () {
-        let options= theForm.theIngredients.options;
-
-        if (options[2].text != "Ingredient"){
-            let lastOption= new Option();
-            options[options.lenght] = lastOption;
-
-            for(let index = options.lenght - 1; index > 2; index--){
-                currentOption.text = previousOption.text;
-                currentOption.value = previousOption.value;
-            }
-            let option = newOption("ingredients", 2);
-            options[2] = option;
-        } else {
-            alert ("Do you want to have Two Extra Ingredients")
-        }
-    }
-        theForm.btnRemovedIng.addEventListener("click", btnRemoveIngClick);
-        theForm.btnAddIng.addEventListener("click", btnAddIngClick);
+function countCheckboxes() {
+    let a =document.forms["main"];
+    let x = a.querySelectorAll('input[type = "checkbox"]: checked');
+    alert(x.lenght);
+}
 
 
 
