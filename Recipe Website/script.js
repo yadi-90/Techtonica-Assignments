55ad8b222f9adbe73021a57629759a65b894a5cc
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-07-25 11:12:23 -0600

    made a form

--- Recipe Website/script.js
+++ Recipe Website/script.js
@@ -1,11 +1,33 @@
-var theForm = document.theForm;
+let theForm = document.theForm;
+
 
 function btnRemoveIngredientsClick() {
-    var options = theForm.theIngredient.options;
+    let options = theForm.theIngredient.options;
     if (options[2].text == "Ingredient"){
         options[2] = null;
     } else {
         alert (" There is no ingredient in list!");
         }
     }
+    function btnAddIngredientsClick () {
+        let options= theForm.theIngredients.options;
+
+        if (options[2].text != "Ingredient"){
+            let lastOption= new Option();
+            options[options.lenght] = lastOption;
+
+            for(let index = options.lenght - 1; index > 2; index--){
+                currentOption.text = previousOption.text;
+                currentOption.value = previousOption.value;
+            }
+            let option = newOption("ingredients", 2);
+            options[2] = option;
+        } else {
+            alert ("Do you want to have Two Extra Ingredients")
+        }
+    }
+        theForm.btnRemovedIng.addEventListener("click", btnRemoveIngClick);
+        theForm.btnAddIng.addEventListener("click", btnAddIngClick);
+
+
 
