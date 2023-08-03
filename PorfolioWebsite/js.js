2f5bc6d021ffb4f8edfa47375b524a867579d09e
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-03 16:09:37 -0600

    connected js sheet

--- PorfolioWebsite/js.js
+++ PorfolioWebsite/js.js
@@ -1,16 +1,23 @@
-let button = document.getElementById("submit");
+const button = document.getElementById("button");
 button.addEventListener("click", (event) => {
-  event.preventDefault();
-    //validation
-  // let userput=username = document.getElementId('username')
-  //userInput.setAttribute('required', 'required')
-  const email = document.getElementById("email").value;
-  const name = document.getElementById("name").value
-    //console.log(username);
-  //console.log(email);
-//const firstErrorDiv = document.getElementByClassName("error")[0];
-  if (email == "" || name == "" ){
-    alert("field mandatory");// firstErrorDiv.innerText = "Please enter Both fields"
-  } else{
-    // firstErrorDiv.innerText = "";
-    console.log(`email:${email} name:${name}`)
+    event.preventDefault();
+
+    const emailInput = document.getElementById("email");
+    const nameInput = document.getElementById("name");
+
+    const email = emailInput.value;
+    const name = nameInput.value;
+
+    if (email === "" || name === "") {
+        alert("Fields are mandatory");
+    } else {
+        console.log(`email: ${email} name: ${name}`);
+        // You can add additional code here to submit the form or perform other actions.
+    }
+});
+
+
+
+
+
+
