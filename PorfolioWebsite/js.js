7801af2f83124581ca4e9402ecec8817dd6ded65
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-04 15:41:55 -0600

    fix the display error on my code

--- PorfolioWebsite/js.js
+++ PorfolioWebsite/js.js
@@ -4,11 +4,20 @@ button.addEventListener("click", (event) => {
 
     const emailInput = document.getElementById("email");
     const nameInput = document.getElementById("name");
+    const textareaInput = document.getElementById("textBox");
 
     const email = emailInput.value;
     const name = nameInput.value;
+    const textarea = textareaInput.value;
 
-    console.log(`email: ${email} name: ${name}`);
+    const errorMessage = document.getElementById("error-message");
+    if (!email || !name || !textarea) {
+        errorMessage.style.display = "block";
+    } else {
+        errorMessage.style.display = "none";
+        console.log(`email: ${email} name: ${name} textarea: ${textarea}`);
+        // Add code here to handle form submission or other actions
+    }
 });
 
 
