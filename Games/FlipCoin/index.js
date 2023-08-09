7d4cb91cf1624b63e8dfbf4aa52b3f909c49e102
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-09 17:00:51 -0600

    made new folder downloaded files github branch

---
+++ Games/FlipCoin/index.js
@@ -0,0 +1,42 @@
+let coin = document.querySelector(".coin");
+let flipBtn=document.querySelector("#flip-button");
+let resetBtn = document.querySelector("#reset-button");
+
+let heads = 0;
+let tails = 0;
+
+flipBtn.addEventListener("click", () =>{
+    let i =Math.floor(Math.random()*2);
+    coin.style.animation ="none";
+
+    if(i){
+        setTimeout(function(){
+        coin.style.animation = "spin-heads 3s forwards";
+    },100); 
+    heads++;
+    }
+    else{
+        setTimeout(function(){
+            coin.style.animation = "spin-tails 3s forwards";
+        },100); 
+        tails++;
+    }
+    setTimeout(updateScore, 3000);
+    disableButton();
+        });
+    resetBtn.addEventListener("click", () => {
+        heads = 0;
+        tails = 0;
+        updateScore();
+    });
+
+    function updateScore(){
+        document.querySelector("#heads-score").textContent = `Heads: ${heads}`;
+        document.querySelector("#tails-score").textContent = `Tails: ${tails}`;}
+
+    function disableButton(){
+        flipBtn.disabled = true;
+        setTimeout(function(){
+            flipBtn.disabled = false;
+        },3000)
+    }
